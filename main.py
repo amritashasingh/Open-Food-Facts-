6703542c9e4bc6809db5edc8a83062e985df046f
Amritasha <69910968+amritashasingh@users.noreply.github.com>
2021-07-07 00:33:01 +0000

    Changed the ingredient

--- main.py
+++ main.py
@@ -21,14 +21,18 @@ with open('ingredients.csv', 'w', encoding='UTF8') as ingredientscsv:
             ingredients = row['ingredients_text']
             #print(ingredients)
 
-            #use regular expression to replace comma with ; when it exists within the ()
-            new_ingredient=re.sub(r'\([^()]+\)', lambda x: x.group().replace(',', ';'), ingredients)
-        
+            #use regular expressions to replace comma with ; when it exists within the ()
+            new_ingredient = re.sub(r'\([^()]+\)',
+                                    lambda x: x.group().replace(',', ';'),
+                                    ingredients)
+
             ingredientssplit = new_ingredient.split(',')
 
             for eachingredient in ingredientssplit:
                 # replaces the semi colons with the original commas
-                new_eachingredient=re.sub(r'\([^()]+\)', lambda x: x.group().replace(';', ','),  eachingredient)
+                new_eachingredient = re.sub(
+                    r'\([^()]+\)', lambda x: x.group().replace(';', ','),
+                    eachingredient)
                 data = [id, new_eachingredient]
                 writerforfile.writerow(data)
 
@@ -37,52 +41,33 @@ originalcsv.close()
 ingredientscsv.close()
 
 #new header for new file
-header2 = ['id', 'peanutbutter']           
+header2 = ['id', 'ironferroussulfate']
 
-# create a new file called peanutbutter.csv
-with open('peanutbutter.csv', 'w', encoding = 'UTF8') as peanutbuttercsv:
-  writerforfile = csv.writer(peanutbuttercsv)
-  writerforfile.writerow(header2)
+# create a new file called ironferroussulfate.csv
+with open('ironferroussulfate.csv', 'w', encoding='UTF8') as ironferroussulfatecsv:
+    writerforfile = csv.writer(ironferroussulfatecsv)
+    writerforfile.writerow(header2)
 
-  #reopens the ingredients.csv file
-  with open('ingredients.csv', 'r', newline = '') as ingredientscsv:
-    reader2 = csv.DictReader(ingredientscsv)
+    #reopens the ingredients.csv file
+    with open('ingredients.csv', 'r', newline='') as ingredientscsv:
+        reader2 = csv.DictReader(ingredientscsv)
 
-    for row in reader2:
-      id = row['id']
-      peanutbutter = row['ingredients']
+        for row in reader2:
+            id = row['id']
+            ironferroussulfate = row['ingredients']
 
-      # if the ingredient in the ingredients.csv file contains peanut butter
-      if "peanut butter" in row['ingredients']:
-        data2 = [id, peanutbutter]
-        writerforfile.writerow(data2)
+            # if the ingredient in the ingredients.csv file contains peanut butter
+            if "iron" in row['ingredients']:
+                data2 = [id, ironferroussulfate]
+                writerforfile.writerow(data2)
+                print()
 
-#--------------------------------------------------------------------------------
+ingredientscsv.close()
 
+# looking for different formatting differences and the number of times a term shows up
 
-
-
-       
-
-
-
-
-
-
-
-  
-
-
-
-
-  
-
-
-
-
-
-
-            
-
-
+#amount of times iron as a term shows up
+#amount of times ferrous sulfate as a term shows up
+#amount of times that reduced iron shows up
+#amount of times that electrolytic iron show up
 
